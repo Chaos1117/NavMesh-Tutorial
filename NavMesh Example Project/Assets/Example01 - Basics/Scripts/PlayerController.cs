d1270d03f18a452a51361de14372b53f98e4ed14
Chaos_04 <kelseylou1117@gmail.com>
2026-02-18 18:54:17 -0500

    Completed all tutorials

--- NavMesh Example Project/Assets/Example01 - Basics/Scripts/PlayerController.cs
+++ NavMesh Example Project/Assets/Example01 - Basics/Scripts/PlayerController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class PlayerController : MonoBehaviour {
 
 	public Camera cam;
 
 	public NavMeshAgent agent;
+
+	public ThirdPersonCharacter character;
+
+	void Start()
+	{
+		agent.updateRotation = false;
+	}
 
 
 	// Update is called once per frame
@@ -23,6 +31,15 @@ public class PlayerController : MonoBehaviour {
 				//move our agent
 				agent.SetDestination(hit.point);
 			}
+		}
+
+		if(agent.remainingDistance > agent.stoppingDistance)
+		{
+			character.Move(agent.desiredVelocity, false, false);
+		}
+		else
+		{
+			character.Move(Vector3.zero, false, false);
 		}
 	}
 }
